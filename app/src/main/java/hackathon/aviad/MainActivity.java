9722dc2e71369ad0db2d3dc7df0e5aff8b4e9ed6
Aviad Katani <aviadkatani@gmail.com>
2016-11-15 14:23:44 +0200

    Add intro

--- app/src/main/java/hackathon/aviad/MainActivity.java
+++ app/src/main/java/hackathon/aviad/MainActivity.java
@@ -2,12 +2,20 @@ package hackathon.aviad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    PrefManager pref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = new PrefManager(this);
+    }
+
+    public void prefReset(View view) {
+        pref.setFirstTimeLaunch(true);
     }
 }
---
+++ app/src/main/java/hackathon/aviad/PrefManager.java
@@ -0,0 +1,34 @@
+package hackathon.aviad;
+
+/**
+ * Created by Aviad on 15/11/2016.
+ */
+import android.content.Context;
+import android.content.SharedPreferences;
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    // Shared Pref Mode:
+    int PRIVATE_MODE = 0;
+    // Shared preferences file name
+    private static final String prefName = "garbagecollector-welcome";
+
+    private static final String isFirstTimeLaunch = "isFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences(prefName, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(isFirstTimeLaunch,isFirstTime);
+        editor.commit();
+    }
+
+    public boolean getIsFirstTimeLaunch() {
+        return pref.getBoolean(isFirstTimeLaunch, true);
+    }
+}
